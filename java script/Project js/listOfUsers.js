0a32445e241b2e1c2686b82f2cb3422826163aa8
MomontD <momontd@gmail.com>
2022-12-20 17:44:05 +0200

    З другою і третоьою (пустою) сторінкою безе переходу з другої Кнопка не передає ід юззера і поста разом в урл

---
+++ java script/Project js/listOfUsers.js
@@ -0,0 +1,24 @@
+
+let mainDiv = document.querySelector(".mainDivOfUsers")
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(inCommingData => inCommingData.json())
+    .then( arrayOfUsers =>
+    {
+        arrayOfUsers.forEach( user =>
+        {
+            userDiv = document.createElement("div")
+            userDiv.classList.add("userDiv")
+            userDiv.innerText = `Name : ${user.name}
+                                           ID   : ${user.id}`
+
+            buttonDiv = document.createElement("div")
+            button = document.createElement("button")
+            button.innerText = "detailed information of user"
+            button.setAttribute("onclick",`document.location='user-details.html?id='+ ${user.id}`)
+
+            buttonDiv.appendChild(button)
+            userDiv.appendChild(buttonDiv)
+            mainDiv.appendChild(userDiv)
+        })
+    })
